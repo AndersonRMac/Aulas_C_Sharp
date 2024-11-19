c8949cccf3ea7ee22b6c3f7312c5e09937ae4716
Your Name <anderson.ramosmaciel@gmail.com>
2024-11-19 11:34:39 -0300

    Primeiro Commit

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+int number = 2;
+
+Console.WriteLine(number += 2);
+
+Console.WriteLine(number -= 3);
---
+++ conversao_implicita_e_casting/aula.cs
@@ -0,0 +1,17 @@
+namespace aula
+{
+    class aula
+    {
+        static void Main(string[] args)
+        {
+
+        double num1;
+        float num2;
+
+        num1 = 5.1;
+        num2 = num1;
+
+        System.Console.WriteLine(num1);
+        }  
+    }
+}
