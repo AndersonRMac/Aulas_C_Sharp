1015e250bca4328dcfe2a3234fe6ec0cc8aa25c7
Your Name <anderson.ramosmaciel@gmail.com>
2025-01-27 11:06:04 -0300

    Convert boolean values to strings 'Yes' or 'No'.

--- Desafios_Code_Wars/NumberConvert.cs
+++ Desafios_Code_Wars/NumberConvert.cs
@@ -7,5 +7,13 @@ namespace CodeWars
         return int.Parse(s);
         }
         
+        //Convert boolean values to strings 'Yes' or 'No'.
+        //Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+        public static string boolToWord(bool word)
+        {
+            return word ? "Yes" : "No";
+        }
+
+
     }  
 }
