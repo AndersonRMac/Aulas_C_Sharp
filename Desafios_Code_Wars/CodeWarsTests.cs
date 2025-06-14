f6c71066a0b0300a4c44e71be035e44557652f46
Your Name <anderson.ramosmaciel@gmail.com>
2025-06-14 10:25:56 -0300

    task relógio

--- Desafios_Code_Wars/CodeWarsTests.cs
+++ Desafios_Code_Wars/CodeWarsTests.cs
@@ -2,11 +2,12 @@ namespace CodeWars
 {
     public class CodeWarsTests
     {
-            //CONVERT A STRING TO A NUMBER
-        public static int StringToNumber(string s){
-        return int.Parse(s);
+        //CONVERT A STRING TO A NUMBER
+        public static int StringToNumber(string s)
+        {
+            return int.Parse(s);
         }
-        
+
         //Convert boolean values to strings 'Yes' or 'No'.
         //Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
         public static string boolToWord(bool word)
@@ -17,12 +18,14 @@ namespace CodeWars
         //Sum Arrays
         /*Write a function that takes an array of numbers and returns the sum of the numbers. The numbers can be negative or non-integer.
          If the array does not contain any numbers then you should return 0.*/
-         public static double SumArray (double[] numbers)
-         {
+        public static double SumArray(double[] numbers)
+        {
             if (numbers.Length == 0)
             {
                 return 0;
-            }else{
+            }
+            else
+            {
                 double cont = 0;
                 for (int i = 0; i < numbers.Length; i++)
                 {
@@ -30,10 +33,10 @@ namespace CodeWars
                 }
                 return cont;
             }
-         }
+        }
 
         //NumberToString
-         public static string NumberToString(int num)
+        public static string NumberToString(int num)
         {
             return num.ToString();
         }
@@ -45,32 +48,42 @@ If he doesn't get 10 hoops, return the string "Keep at it until you get it".*/
 
         public static string HoopCount(int n)
         {
-            return (n > 10)?"Great, now move on to tricks" : "Keep at it until you get it";
+            return (n > 10) ? "Great, now move on to tricks" : "Keep at it until you get it";
         }
-/*
-Messi goals function
-Messi is a soccer player with goals in three leagues:LaLiga, Copa del Rey, Champions
-Complete the function to return his total number of goals in all three leagues.
-Note: the input will always be valid.
-For example:
-5, 10, 2  -->  17
-*/
+        /*
+        Messi goals function
+        Messi is a soccer player with goals in three leagues:LaLiga, Copa del Rey, Champions
+        Complete the function to return his total number of goals in all three leagues.
+        Note: the input will always be valid.
+        For example:
+        5, 10, 2  -->  17
+        */
         public static int GetGoals(int laLigaGoals, int copaDelReyGoals, int championsLeagueGoals)
         {
-            
+
             int total = laLigaGoals + copaDelReyGoals + championsLeagueGoals;
             return total;
-            
+
         }
 
-/*
-The cockroach is one of the fastest insects. Write a function which takes its speed in km per hour and returns
- it in cm per second, rounded down to the integer (= floored).*/
-    public static int CockroachSpeed(double x)
-    {
-      int ConvertValor = (int)(x * 27.7778);
-      return ConvertValor;
-    }
-    }
+        /*
+        The cockroach is one of the fastest insects. Write a function which takes its speed in km per hour and returns
+         it in cm per second, rounded down to the integer (= floored).*/
+        public static int CockroachSpeed(double x)
+        {
+            int ConvertValor = (int)(x * 27.7778);
+            return ConvertValor;
+        }
 
+        /*
+        Clock shows h hours, m minutes and s seconds after midnight.
+        Your task is to write a function which returns the time since midnight in milliseconds.*/
+        public static int relogio(int h, int m, int s)
+        {
+            h = *3600000;
+            m = *60000;
+            s = *1000;
+            return h + m + s;
+        }
+    }
 }
